c2c425dbf0c15235a723f3495285e91980aae4b8
Nachovy <xs21@rice.edu>
2023-03-21 04:53:12 +0000

    1.0

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='NCBIQuery',
-    version='0.1',
+    version='1.0',
     license='Delt4',
     author="Leo Sun",
     author_email='zidaneandmessi@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),
     url='https://github.com/nachovy/NCBIQuery',
     keywords='NCBI',
     install_requires=[
           'html',
           'requests',
-          'xml',
           'tokenizers',
       ],
 
